138216b1d76193db5d32cffffa39129da5471f8d
Minung Han <hmu332233@gmail.com>
2022-08-14 19:02:36 +0900

    refactor: 불필요한 코드 제거

--- src/Minung.tsx
+++ src/Minung.tsx
@@ -1,45 +1,30 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { Box, Text, Newline } from 'ink';
+import { Box, Text } from 'ink';
 
 import useTypingEffect from './useTypingEffect';
 
+const TEXTS = [
+  `Hi! I'm Minung!`,
+  `I'm working as a software engineer at Goorm.`,
+  ``,
+  `Github: https://github.com/hmu332233`,
+  `BuyMeACoffe: https://www.buymeacoffee.com/minungHan`,
+].join('\n');
+
 function Minung() {
-  const { displayText: firstText, end: firstTextEnd, setPlay: setFirstTextPlay } = useTypingEffect({
-    text: "Hi! I'm Minung!",
-  });
-  const { displayText: secondText, end: secondTextEnd, setPlay: setSecondTextPlay } = useTypingEffect({
-    text: "I'm working as a software engineer at Goorm.",
-  });
-  const { displayText: thirdText, setPlay: setThirdTextPlay } = useTypingEffect({
-    text: "Github: https://github.com/hmu332233",
+  const { displayText, setPlay } = useTypingEffect({
+    text: TEXTS,
   });
 
   useEffect(() => {
-    setFirstTextPlay(true);
-  }, [])
-
-  useEffect(() => {
-    if (firstTextEnd) {
-      setSecondTextPlay(true);
-    }
-  }, [firstTextEnd]);
-
-  useEffect(() => {
-    if (secondTextEnd) {
-      setThirdTextPlay(true);
-    }
-  }, [secondTextEnd]);
+    setPlay(true);
+  }, []);
 
   return (
     <Box flexDirection="column" padding={2}>
       <Text>
-        {firstText}
-        <Newline />
-        {secondText}
-        <Newline />
-        <Newline />
-        {thirdText}
+        {displayText}
       </Text>
     </Box>
   );
